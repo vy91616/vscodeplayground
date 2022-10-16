c5e936a67717a5cc238e4c114bdb53b71d37fcf2
Vinod Yadav <vy91616@gmail.com>
2022-10-16 13:54:37 +0530

    16aug

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
+    clickCreateItem : function(component, event) {
+        let validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        if(validExpense) {
+            let newCamping = component.get("v.newItem");
 
+        }
     }
 })
